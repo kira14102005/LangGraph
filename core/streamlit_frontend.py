091952a7a3a0498a84d168be2561601386319176
Harshit Rai <raiharshit66@gmail.com>
2026-09-12 22:53:19 +0530

    fix:Remove relative imports from the streamlit frontend and for now remove the use from utils

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -1,15 +1,17 @@
 import streamlit as st
-
 user_input = st.chat_input('Type your message here...')
 
-message_history = []
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+chat_history = st.session_state['chat_history']
 
 if user_input:
-    message_history.append({"role": "user", "content": user_input})
+    st.session_state['chat_history'].append({"role": "user", "content": user_input})
 
     ai_response = user_input
-    message_history.append({"role": "assistant", "content": ai_response})
+    st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
 
-for message in message_history:
+for message in chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
