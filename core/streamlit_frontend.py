92e82684091786e837ede1a621fc2bdf27779109
Harshit Rai <raiharshit66@gmail.com>
2026-09-12 23:29:46 +0530

    refactor:Move the main executional code to its own block separating out the stream_token_generator()

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -2,23 +2,9 @@ import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langgraph_backend import workflow
-
-user_input = st.chat_input('Type your message here...')
-
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-
-chat_history = st.session_state['chat_history']
-thread_id = "user_1"
-config = {"configurable": {"thread_id": thread_id}}
-
-for message in chat_history:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
 import time
 
-def generate():
+def stream_token_generator():
     for chunk, metadata in workflow.stream(
         {"messages": [HumanMessage(content=user_input)]},
         config=config,
@@ -31,13 +17,32 @@ def generate():
                 yield word + " "
                 time.sleep(0.05)
 
-if user_input:
-    st.session_state['chat_history'].append({"role": "user", "content": user_input})
-    with st.chat_message("user"):
-            st.markdown(user_input)
 
-    with st.chat_message("assistant"):
-        ai_response = st.write_stream(generate())
-    
-    st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
-    
+if __name__ == "__main__":
+    st.set_page_config(page_title="LangGraph Chatbot", page_icon=":robot:")
+    st.title("LangGraph Chatbot")
+
+    user_input = st.chat_input('Type your message here...')
+
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+
+    chat_history = st.session_state['chat_history']
+    thread_id = "user_1"
+    config = {"configurable": {"thread_id": thread_id}}
+
+    for message in chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+    if user_input:
+        st.session_state['chat_history'].append({"role": "user", "content": user_input})
+        with st.chat_message("user"):
+                st.markdown(user_input)
+
+        with st.chat_message("assistant"):
+            ai_response = st.write_stream(stream_token_generator())
+        
+        st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
+        
