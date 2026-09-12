c0cb9625dd96421bfecddd74a8adbae87b688e9c
Harshit Rai <raiharshit66@gmail.com>
2026-09-12 23:03:19 +0530

    add:Add the configurable{ thread_id } parameter to invoke function so that MemoryState is registered to that id

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -1,17 +1,30 @@
 import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langgraph_backend import workflow
+
 user_input = st.chat_input('Type your message here...')
 
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 
 chat_history = st.session_state['chat_history']
-
-if user_input:
-    st.session_state['chat_history'].append({"role": "user", "content": user_input})
-
-    ai_response = user_input
-    st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
+thread_id = "user_1"
+config = {"configurable": {"thread_id": thread_id}}
 
 for message in chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if user_input:
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    st.session_state['chat_history'].append({"role": "user", "content": user_input})
+
+    ai_response = workflow.invoke({"messages" : [HumanMessage(content=user_input)]}, config=config)['messages'][-1].content
+
+    st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(ai_response)
