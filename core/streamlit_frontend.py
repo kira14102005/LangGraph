0568994a3e26b9f816b2adbd4226c7968a5e5dc9
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 16:36:45 +0530

    add:Add a sidebar to hold the conversations at one place

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -33,17 +33,20 @@ def stream_token_generator():
 
 if __name__ == "__main__":
     st.set_page_config(page_title="LangGraph Chatbot", page_icon=":robot:")
-    st.title("LangGraph Chatbot")
 
     user_input = st.chat_input('Type your message here...')
 
+    st.sidebar.title("LangGraph Chatbot")
+    st.sidebar.button("New Chat")
+    st.sidebar.header("Your Chats")
+    
     if 'chat_history' not in st.session_state:
         st.session_state['chat_history'] = []
 
     chat_history = st.session_state['chat_history']
     thread_id = "user_1"
     config = {"configurable": {"thread_id": thread_id}}
-
+    
     for message in chat_history:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
