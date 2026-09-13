6e19749aec2af39ad024e203e1b8176cb9f9eb5a
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 19:01:18 +0530

    update:Update the frontend part to load threads, update streamlit.session_state according to the SQLite db

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessageChunk
-from langgraph_backend_sqlite import workflow
+from langgraph_backend_sqlite import workflow, fetch_all_thread_ids
 from utils.generate_thread_id import generate_thread_id
 from utils.reset_chat import reset_chat
 from utils.add_thread import add_thread_to_history
 from utils.render_threads import render_chat_threads
+from utils.load_chat_history import load_chat_history_from_state
 import time
 import logging
 
@@ -14,17 +15,18 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def stream_token_generator():
+
+def stream_token_generator(user_input: str, config: dict):
     for chunk, metadata in workflow.stream(
         {"messages": [HumanMessage(content=user_input)]},
-        config=CONFIG,
+        config=config,
         stream_mode="messages",
     ):
         logger.info(
-        "NODE = %s | TYPE = %s | TEXT = %r",
-        metadata.get("langgraph_node"),
-        type(chunk).__name__,
-        chunk.text,
+            "NODE = %s | TYPE = %s | TEXT = %r",
+            metadata.get("langgraph_node"),
+            type(chunk).__name__,
+            chunk.text,
         )
         text = chunk.text
 
@@ -35,19 +37,20 @@ def stream_token_generator():
 
 
 if __name__ == "__main__":
-    if 'chat_history' not in st.session_state:
-            st.session_state['chat_history'] = []
-    
-    if 'thread_id' not in st.session_state:
-        st.session_state['thread_id'] = generate_thread_id()
-    
-    if 'chat_threads' not in st.session_state:
-        st.session_state['chat_threads'] = []
-    
-    add_thread_to_history(st.session_state['thread_id'])
     st.set_page_config(page_title="LangGraph Chatbot", page_icon=":robot:")
 
-    user_input = st.chat_input('Type your message here...')
+    stored_threads = fetch_all_thread_ids()
+    stored_threads.reverse()
+
+    if 'thread_id' not in st.session_state:
+        st.session_state['thread_id'] = generate_thread_id()
+
+    if 'chat_threads' not in st.session_state:
+        st.session_state['chat_threads'] = stored_threads or []
+    
+    st.session_state['chat_history'] = load_chat_history_from_state(st.session_state['thread_id'])
+
+    add_thread_to_history(st.session_state['thread_id'])
 
     st.sidebar.title("LangGraph Chatbot")
     if st.sidebar.button("New Chat"):
@@ -56,22 +59,21 @@ if __name__ == "__main__":
     st.sidebar.header("Your Chats")
     render_chat_threads()
 
-    CONFIG = {"configurable" : {'thread_id' : st.session_state['thread_id']}}
+    CONFIG = {"configurable": {'thread_id': st.session_state['thread_id']}}
 
-    chat_history = st.session_state['chat_history']
-    
-    for message in chat_history:
+    for message in st.session_state['chat_history']:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-
+    user_input = st.chat_input('Type your message here...')
     if user_input:
         st.session_state['chat_history'].append({"role": "user", "content": user_input})
         with st.chat_message("user"):
-                st.markdown(user_input)
+            st.markdown(user_input)
 
         with st.chat_message("assistant"):
-            ai_response = st.write_stream(stream_token_generator())
-        
+            ai_response = st.write_stream(stream_token_generator(user_input, CONFIG))
+
         st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
-        
+
+        st.rerun()
