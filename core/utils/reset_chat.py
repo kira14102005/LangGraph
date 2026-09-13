8114de904de15205a5c5fcf53ae8fd141514eb56
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 17:12:04 +0530

    edit-fix:Add a check to the reset check only create a new thread if the existing one has some chats and needs to be cleared

--- core/utils/reset_chat.py
+++ core/utils/reset_chat.py
@@ -3,6 +3,8 @@ from .generate_thread_id import generate_thread_id
 from .add_thread import add_thread_to_history
 
 def reset_chat():
+    if not st.session_state['chat_history'] or len(st.session_state['chat_history']) == 0:
+        return
     thread_id = generate_thread_id()
     st.session_state['thread_id'] = thread_id
     st.session_state['chat_history'] = []
