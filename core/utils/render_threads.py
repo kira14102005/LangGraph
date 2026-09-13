c2b09fc5662577010c0c6476632fb55e6687afef
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 17:52:27 +0530

    change:Divide the render_chat_threads into  substitute helper functions that add up to do the task -- 1)Rendering the Chat Threads in the SideBar 2)OnClick update of session_state['thread_id'] 3)Call the update_chat_history() and set the updated history in session_state

--- core/utils/render_threads.py
+++ core/utils/render_threads.py
@@ -1,23 +1,14 @@
 import streamlit as st
+from .update_chat_history import update_chat_history
 from langgraph_backend import workflow
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 def render_chat_threads():
     for thread_id in reversed(st.session_state['chat_threads']):
         if st.sidebar.button(thread_id):
-            st.session_state['thread_id'] = thread_id
-            config = {"configurable" : {"thread_id" : thread_id}}
-            chat_history = []
-            state_snapshot = workflow.get_state(config).values
-            # print(state_snapshot)
-            for message in state_snapshot['messages']:
-                if type(message) == HumanMessage:
-                    role = 'user'
-                elif type(message) == AIMessage:
-                    role= 'assistant'
-                else:
-                    role = 'system'
+            click_thread(thread_id)
 
-                chat_history.append({'role' : role, 'content' : message.content})
-
-            st.session_state['chat_history']  = chat_history
+def click_thread(thread_id):
+    st.session_state['thread_id'] = thread_id
+    updated_chat_history = update_chat_history(thread_id)
+    st.session_state['chat_history'] = updated_chat_history
