9d13317d3d84ab3aecf7824cad69f517bb0039d4
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 17:39:01 +0530

    fix:Fix the message structure and add the chat_history corresponding to the thread_idwhen that id is clicked

--- core/utils/render_threads.py
+++ core/utils/render_threads.py
@@ -1,5 +1,23 @@
 import streamlit as st
+from langgraph_backend import workflow
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 def render_chat_threads():
     for thread_id in reversed(st.session_state['chat_threads']):
-        st.sidebar.text(thread_id)
+        if st.sidebar.button(thread_id):
+            st.session_state['thread_id'] = thread_id
+            config = {"configurable" : {"thread_id" : thread_id}}
+            chat_history = []
+            state_snapshot = workflow.get_state(config).values
+            # print(state_snapshot)
+            for message in state_snapshot['messages']:
+                if type(message) == HumanMessage:
+                    role = 'user'
+                elif type(message) == AIMessage:
+                    role= 'assistant'
+                else:
+                    role = 'system'
+
+                chat_history.append({'role' : role, 'content' : message.content})
+
+            st.session_state['chat_history']  = chat_history
