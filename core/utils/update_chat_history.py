d22499c13458c5993268416e5c781a8a4ce05c3f
Harshit Rai <raiharshit66@gmail.com>
2026-09-13 17:53:31 +0530

    fix:Use safe fallbacks for newly created threads, by using dict.get( , )

--- core/utils/update_chat_history.py
+++ core/utils/update_chat_history.py
@@ -5,9 +5,9 @@ from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 def update_chat_history(thread_id):
     chat_history = []
     config = {"configurable" : {"thread_id" : thread_id}}
-    state_snapshot_me = workflow.get_state(config).values
-    # print(state_snapshot)
-    for message in state_snapshot['messages']:
+    state_snapshot = workflow.get_state(config)
+    updated_chat_history = state_snapshot.values or {}
+    for message in updated_chat_history.get('messages', []):
         if type(message) == HumanMessage:
             role = 'user'
         elif type(message) == AIMessage:
