3b405f790db04f09439d8fd0d93532de1f95b9bd
Harshit Rai <raiharshit66@gmail.com>
2026-09-14 23:18:53 +0530

    fix:Since we don't have parser in the backend now, extract the text part from AIMessage Content

--- core/utils/load_chat_history.py
+++ core/utils/load_chat_history.py
@@ -5,11 +5,12 @@ def load_chat_history_from_state(thread_id: str):
     config = {"configurable": {"thread_id": thread_id}}
     state = workflow.get_state(config)
     messages = state.values.get("messages", [])
-    
     formatted_history = []
     for msg in messages:
-        if isinstance(msg, HumanMessage):
+        if not msg.content:
+            continue
+        elif isinstance(msg, HumanMessage):
             formatted_history.append({"role": "user", "content": msg.content})
         elif isinstance(msg, AIMessage):
-            formatted_history.append({"role": "assistant", "content": msg.content})
+            formatted_history.append({"role": "assistant", "content": msg.content[0]['text'] if isinstance(msg.content, list) and msg.content and isinstance(msg.content[0], dict) and 'text' in msg.content[0] else msg.content})
     return formatted_history
