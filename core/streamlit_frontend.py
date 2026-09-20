18dbd01741f5fa8e4da349ec35e7dad59f197d57
Harshit Rai <raiharshit66@gmail.com>
2026-09-20 15:34:30 +0530

    change:Move all the async code from main block to a separate function

--- core/streamlit_frontend.py
+++ core/streamlit_frontend.py
@@ -2,7 +2,7 @@ import asyncio
 
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessageChunk
-from async_langgraph_chatbot import workflow, fetch_all_thread_ids
+from async_langgraph_chatbot import build_workflow, fetch_all_thread_ids
 from utils.generate_thread_id import generate_thread_id
 from utils.reset_chat import reset_chat
 from utils.add_thread import add_thread_to_history
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def stream_token_generator(user_input: str, config: dict):
+async def stream_token_generator(workflow, user_input: str, config: dict):
     async for chunk, metadata in workflow.astream(
         {"messages": [HumanMessage(content=user_input)]},
         config=config,
@@ -40,19 +40,20 @@ async def stream_token_generator(user_input: str, config: dict):
 
 # TOOLMESSAGE : 2026-09-14 23:08:11,145 - __main__ - INFO - NODE = tools | TYPE = ToolMessage | TEXT = '{"Global Quote": {"01. symbol": "AXP", "02. open": "324.9700", "03. high": "326.2600", "04. low": "322.2100", "05. price": "324.6900", "06. volume": "1930287", "07. latest trading day": "2026-09-11", "08. previous close": "320.7100", "09. change": "3.9800", "10. change percent": "1.2410%"}}'
 
-if __name__ == "__main__":
-    st.set_page_config(page_title="LangGraph Chatbot", page_icon=":robot:")
-
-    stored_threads = fetch_all_thread_ids()
+async def app():
+    workflow, conn = await build_workflow()
+    
+    stored_threads = await fetch_all_thread_ids(conn)
     stored_threads.reverse()
 
+    st.set_page_config(page_title="LangGraph Chatbot", page_icon=":robot:")
     if 'thread_id' not in st.session_state:
-        st.session_state['thread_id'] = generate_thread_id()
-
+            st.session_state['thread_id'] = generate_thread_id()
+    
     if 'chat_threads' not in st.session_state:
         st.session_state['chat_threads'] = stored_threads or []
     
-    st.session_state['chat_history'] = load_chat_history(st.session_state['thread_id'])
+    st.session_state['chat_history'] = await load_chat_history(workflow, st.session_state['thread_id'])
 
     add_thread_to_history(st.session_state['thread_id'])
 
@@ -61,7 +62,7 @@ if __name__ == "__main__":
         reset_chat()
 
     st.sidebar.header("Your Chats")
-    render_chat_threads()
+    await render_chat_threads(workflow=workflow)
 
     CONFIG = {"configurable": {'thread_id': st.session_state['thread_id']}}
 
@@ -76,7 +77,9 @@ if __name__ == "__main__":
             st.markdown(user_input)
 
         with st.chat_message("assistant"):
-            ai_response = st.write_stream(stream_token_generator(user_input, CONFIG))
+            ai_response = st.write_stream(stream_token_generator(workflow, user_input, CONFIG))
         st.session_state['chat_history'].append({"role": "assistant", "content": ai_response})
 
         st.rerun()
+if __name__ == "__main__":
+    asyncio.run(app())   
